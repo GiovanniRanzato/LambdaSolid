d32e9b88557fe94e724600ae359502bbd440838a
giovanni.ranzato <giovanni.ranzato@wecanconsulting.it>
2025-05-10 17:01:51 +0200

    Structure refactor

--- tests/unit/api/api_gateway/events/test_APIGatewayEventBase.py
+++ tests/unit/api/api_gateway/events/test_APIGatewayEventBase.py
@@ -1,8 +1,8 @@
 import pytest
-from api.api_gateway.APIGatewayEvent import APIGatewayEvent
+from api.api_gateway.events.APIGatewayEventBase import APIGatewayEventBase
 
 
-class TestAPIGatewayEvent:
+class TestAPIGatewayEventBase:
     @pytest.fixture
     def event_dict(self):
         return {
@@ -18,16 +18,16 @@ class TestAPIGatewayEvent:
 
     @pytest.fixture
     def api_gateway_event(self, event_dict, event_context):
-        return APIGatewayEvent(event=event_dict, context=event_context)
+        return APIGatewayEventBase(event=event_dict, context=event_context)
 
     def test_init(self, api_gateway_event, event_dict, event_context):
-        assert isinstance(api_gateway_event, APIGatewayEvent)
+        assert isinstance(api_gateway_event, APIGatewayEventBase)
         assert api_gateway_event.event == event_dict
         assert api_gateway_event.context == event_context
 
     def test_from_dict(self, event_dict, event_context):
-        api_gateway_event = APIGatewayEvent.from_dict(event=event_dict, context=event_context)
-        assert isinstance(api_gateway_event, APIGatewayEvent)
+        api_gateway_event = APIGatewayEventBase.from_dict(event=event_dict, context=event_context)
+        assert isinstance(api_gateway_event, APIGatewayEventBase)
         assert api_gateway_event.event == event_dict
         assert api_gateway_event.context == event_context
 
@@ -36,11 +36,11 @@ class TestAPIGatewayEvent:
         [({"routeKey": "valid value", "requestContext": "valid value"}, True), ({"invalid": "invalid"}, False)],
     )
     def test_is_valid(self, events, is_valid):
-        assert APIGatewayEvent.is_valid(events) is is_valid
+        assert APIGatewayEventBase.is_valid(events) is is_valid
 
     def test_get_event_type(self, api_gateway_event):
-        assert api_gateway_event.get_event_type() == "APIGatewayEvent"
+        assert api_gateway_event.get_event_type() == "APIGatewayEventBase"
 
     def test_invalid_event(self):
         with pytest.raises(ValueError, match="Invalid event format for APIGatewayEvent: {'invalid_event': 'value'}"):
-            APIGatewayEvent.from_dict(event={"invalid_event": "value"}, context={})
+            APIGatewayEventBase.from_dict(event={"invalid_event": "value"}, context={})
