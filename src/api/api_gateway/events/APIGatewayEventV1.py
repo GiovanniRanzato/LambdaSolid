5d3d08e46c5518f4a9c6ebaea39469171f4f2d92
giovanni.ranzato <giovanni.ranzato@wecanconsulting.it>
2025-05-15 17:35:08 +0200

    Add APIGatewayEventV1.py

---
+++ src/api/api_gateway/events/APIGatewayEventV1.py
@@ -0,0 +1,21 @@
+from api.api_gateway.events.APIGatewayEventBase import APIGatewayEventBase
+from api.api_gateway.interfaces.APIGatewayRequestI import APIGatewayRequestI
+
+
+class APIGatewayEventV1(APIGatewayRequestI, APIGatewayEventBase):
+    def __init__(self, event: dict, context: dict | None):
+        super().__init__(event, context)
+        self.version = "v1"
+
+    def get_body(self) -> dict:
+        return self.event
+
+    def get_context(self):
+        return self.context
+
+    def get_stage(self) -> str:
+        return self.get_body().get('requestContext', {}).get('stage', '')
+
+    def get_version(self) -> str:
+        return self.version
+
---
+++ tests/unit/api/api_gateway/events/test_APIGatewayEventV1.py
@@ -0,0 +1,38 @@
+import pytest
+
+from api.api_gateway.events.APIGatewayEventV1 import APIGatewayEventV1
+from unit.api.api_gateway.events.test_APIGatewayEventBase import TestAPIGatewayEventBase
+
+
+class TestAPIGatewayEventV1(TestAPIGatewayEventBase):
+    @pytest.fixture
+    def event_dict(self):
+        return {
+            "resource": "/{proxy+}",
+            "httpMethod": "POST",
+            "requestContext": {
+                "stage": "default"
+            }
+        }
+
+    @pytest.fixture
+    def event(self, event_dict, event_context) -> APIGatewayEventV1:
+        return APIGatewayEventV1(
+            event=event_dict,
+            context=event_context
+        )
+
+    def test_init_event_v1(self, event):
+        assert isinstance(event, APIGatewayEventV1)
+
+    def test_get_body(self, event):
+        assert event.get_body() == event.event
+
+    def test_get_context(self, event):
+        assert event.get_context() == event.context
+
+    def test_get_stage(self, event, event_dict):
+        assert event.get_stage() == event_dict.get('requestContext', {}).get('stage', '')
+
+    def test_version(self, event):
+        assert event.get_version() == "v1"
