f0f3bfb3c93a7c353792eb42b129b38b5fa70009
giovanni.ranzato <giovanni.ranzato@wecanconsulting.it>
2025-05-15 17:36:40 +0200

    Add ExceptionsHandler

---
+++ src/api/api_gateway/exceptions/handlers/ExceptionHandler.py
@@ -0,0 +1,29 @@
+import logging
+import traceback
+from typing import Type
+
+from starlette import status
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+
+from api.api_gateway.exceptions.handlers.ExceptionHandlerBase import ExceptionHandlerBase
+
+
+class ExceptionHandler(ExceptionHandlerBase):
+    @staticmethod
+    def handled_exception() -> Type[Exception]:
+        return Exception
+
+    @classmethod
+    def handle (cls, request: Request, exception: Exception):
+        error_message = exception.args[0] if exception.args else str(exception)
+        logging.error(traceback.format_exc())
+        logging.error(error_message)
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"detail": f"Something went wrong {error_message}"},
+        )
+
+
+
+
