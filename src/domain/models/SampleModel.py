f2da60fa687019771dc9983f8a6161a9164c835c
giovanni.ranzato <giovanni.ranzato@wecanconsulting.it>
2025-05-23 01:19:51 +0200

    Add DBObjectBase.py

--- src/domain/models/SampleModel.py
+++ src/domain/models/SampleModel.py
@@ -1,14 +1,11 @@
 from dataclasses import dataclass
 from datetime import datetime
 
-from repositories.interfaces.DBObjectI import DBObjectI
+from repositories.db.DBObjectBase import DBObjectBase
 
 @dataclass
-class SampleModel(DBObjectI):
+class SampleModel(DBObjectBase):
     sample_id: str = None
     name: str = None
     created_at: datetime = None
     updated_at: datetime = None
-
-    def model_dump(self) -> dict:
-        return self.__dict__
