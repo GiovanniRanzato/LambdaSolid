34970bfb21c285356ea0fc8f5a3b8a3f67f3249b
giovanni.ranzato <giovanni.ranzato@wecanconsulting.it>
2025-05-22 23:42:10 +0200

    Add DynamoDB.py

---
+++ src/repositories/db/dynamo_db/DynamoDB.py
@@ -0,0 +1,12 @@
+import boto3
+
+from infrastructure.interfaces.ConfigI import ConfigI
+
+
+class DynamoDB:
+    def __init__(self, config: ConfigI):
+        self.resource = boto3.resource(
+            "dynamodb",
+            region_name=config.get("DYNAMODB_REGION"),
+            endpoint_url=config.get("LOCALSTACK_ENDPOINT_URL")
+        )
---
+++ tests/unit/repositories/db/dynamo_db/TestDynamoDB.py
@@ -0,0 +1,27 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from infrastructure.interfaces.ConfigI import ConfigI
+from repositories.db.dynamo_db.DynamoDB import DynamoDB
+
+
+class TestDynamoDB:
+    @pytest.fixture
+    def config(self) -> ConfigI:
+        config = MagicMock(spec=ConfigI)
+        config.get.side_effect = lambda key: {
+            "DYNAMODB_REGION": "eu-west-1",
+            "LOCALSTACK_ENDPOINT_URL": "http://localhost:4566"
+        }.get(key, None)
+        return config
+
+    @pytest.fixture
+    def dynamo_db(self, config):
+        return DynamoDB(config)
+
+    def test_init(self, dynamo_db):
+        assert dynamo_db.resource is not None
+        assert dynamo_db.resource.meta.client._endpoint.host == "http://localhost:4566"
+        assert dynamo_db.resource.meta.client.meta.region_name == "eu-west-1"
+        assert isinstance(dynamo_db, DynamoDB)
