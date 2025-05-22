429965cc4c73a3df9d02b4930d25d9fc90a3261c
giovanni.ranzato <giovanni.ranzato@wecanconsulting.it>
2025-05-23 01:19:39 +0200

    Add DBObjectBase.py

---
+++ src/repositories/db/DBObjectBase.py
@@ -0,0 +1,6 @@
+from repositories.interfaces.DBObjectI import DBObjectI
+
+
+class DBObjectBase(DBObjectI):
+    def model_dump(self) -> dict:
+        return self.__dict__
---
+++ tests/unit/repositories/db/test_DBObject.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+
+import pytest
+
+from repositories.db.DBObjectBase import DBObjectBase
+
+
+class TestDBObject:
+    @pytest.fixture
+    def db_object(self):
+        @dataclass
+        class DummyDBObject(DBObjectBase):
+            id: str
+            name: str
+
+        return DummyDBObject(id="123", name="Test")
+
+    def test_init(self, db_object):
+        assert db_object.id == "123"
+        assert db_object.name == "Test"
+        assert isinstance(db_object, DBObjectBase)
+
+    def test_model_dump(self, db_object):
+        expected_dict = {
+            "id": "123",
+            "name": "Test"
+        }
+        assert db_object.model_dump() == expected_dict
