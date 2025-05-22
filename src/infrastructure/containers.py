c6b38b623b46a51349bdfcf542dee1fe7bf240b2
giovanni.ranzato <giovanni.ranzato@wecanconsulting.it>
2025-05-23 01:54:12 +0200

    Add DB Sample table repository

--- src/infrastructure/containers.py
+++ src/infrastructure/containers.py
@@ -3,6 +3,8 @@ from dependency_injector import containers, providers
 from infrastructure.config.Config import Config
 from infrastructure.factories.EventFactory import EventFactory
 from infrastructure.EventsRegistry import EventsRegistry
+from repositories.db.dynamo_db.DynamoDBTableSample import DynamoDBTableSample
+from repositories.db.dynamo_db.DynamoDBSerializer import DynamoDBSerializer
 
 
 class Container(containers.DeclarativeContainer):
@@ -10,3 +12,6 @@ class Container(containers.DeclarativeContainer):
     config = providers.Singleton(Config)
     events_registry = providers.Singleton(EventsRegistry)
     event_factory = providers.Factory(EventFactory, events_registry=events_registry)
+
+    dynamo_db_serializer = providers.Singleton(DynamoDBSerializer)
+    sample_db_table = providers.Singleton(DynamoDBTableSample, config=config, serializer=dynamo_db_serializer)
