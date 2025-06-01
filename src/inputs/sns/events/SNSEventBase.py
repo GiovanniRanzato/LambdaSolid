43a352baa92414d7ed1108b9bb23b76dc6e05ba8
giovanni.ranzato <giovanni.ranzato@wecanconsulting.it>
2025-06-01 20:29:46 +0200

    Add SNSEventBase

---
+++ src/inputs/sns/events/SNSEventBase.py
@@ -0,0 +1,25 @@
+import json
+
+from infrastructure.interfaces.EventI import EventI
+
+
+class SNSEventBase(EventI):
+    def __init__(self, event: dict):
+        self.event = event
+
+    @classmethod
+    def get_event_type(cls) -> str:
+        return cls.__name__
+
+    @classmethod
+    def from_dict(cls, event: dict, context: dict | None) -> "SNSEventBase":
+        if not cls.is_valid(event):
+            raise ValueError(f"Invalid event format for SNSEventBase: {event}")
+
+        return cls(event=event)
+
+    @classmethod
+    def is_valid(cls, event: dict) -> bool:
+        if event.get("EventSource") != "aws:sns" and event.get("Sns").get("Message") is None:
+            return False
+        return json.loads(event.get("Sns").get("Message")).get("type") == cls.get_event_type()
---
+++ tests/unit/inputs/sns/events/test_SNSEventBase.py
@@ -0,0 +1,45 @@
+import pytest
+
+from infrastructure.interfaces.EventI import EventI
+from inputs.sns.events.SNSEventBase import SNSEventBase
+
+class TestSNSEventBase:
+    @pytest.fixture
+    def sns_event_dict(self):
+        return {
+            "EventSource": "aws:sns",
+            "Sns": {
+                "Message": '{"type": "SNSEventBase"}'
+            }
+        }
+
+    @pytest.fixture
+    def sns_event_base(self, sns_event_dict):
+        return SNSEventBase(
+            event=sns_event_dict
+        )
+
+    def test_init(self, sns_event_base, sns_event_dict):
+        assert isinstance(sns_event_base, SNSEventBase)
+        assert isinstance(sns_event_base, EventI)
+        assert sns_event_base.event == sns_event_dict
+
+    def test_get_event_type(self):
+        assert SNSEventBase.get_event_type() == "SNSEventBase"
+
+    def test_from_dict(self, sns_event_dict):
+        event = SNSEventBase.from_dict(sns_event_dict, None)
+        assert isinstance(event, SNSEventBase)
+        assert event.event == sns_event_dict
+
+    def test_is_valid(self, sns_event_dict):
+        assert SNSEventBase.is_valid(sns_event_dict) is True
+
+    def test_not_valid_event(self):
+        invalid_event = {
+            "EventSource": "aws:sns",
+            "Sns": {
+                "Message": '{"type": "InvalidEventType"}'
+            }
+        }
+        assert SNSEventBase.is_valid(invalid_event) is False
