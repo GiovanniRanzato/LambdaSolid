e7966dfc8334b83b6c01bd6021f31a174292112f
giovanni.ranzato <giovanni.ranzato@wecanconsulting.it>
2025-06-25 11:49:24 +0200

    Add EBEventBase.py

---
+++ src/inputs/event_bridge/events/EBEventBase.py
@@ -0,0 +1,26 @@
+from infrastructure.interfaces.EventI import EventI
+
+
+class EBEventBase (EventI):
+    def __init__(self, event: dict):
+        self.event = event
+
+    @classmethod
+    def get_event_type(cls) -> str:
+        return cls.__name__
+
+    @classmethod
+    def from_dict(cls, event: dict, context: dict | None) -> "EBEventBase":
+        if not cls.is_valid(event):
+            raise ValueError(f"Invalid event format for EBEventBase: {event}")
+
+        return cls(event=event)
+
+    @classmethod
+    def is_valid(cls, event: dict) -> bool:
+        if (event.get("source", None) != "aws.events" or
+                event.get("resources", None) is None or
+                not isinstance(event.get("resources"), list) or
+                event.get("resources")[0] != "arn:aws:events:rule/{}".format(cls.get_event_type())):
+            return False
+        return True
---
+++ tests/unit/inputs/event_bridge/events/test_EBEventBase.py
@@ -0,0 +1,52 @@
+import uuid
+from inputs.event_bridge.events.EBEventBase import EBEventBase
+import pytest
+
+
+class TestEBEventBase:
+    @pytest.fixture
+    def eb_event_base_dict(self):
+        return {
+            "source": "aws.events",
+            "resources": [
+                "arn:aws:events:rule/EBEventBase"
+            ]
+        }
+
+    @pytest.fixture
+    def eb_event_base(self, eb_event_base_dict):
+        return EBEventBase(event=eb_event_base_dict)
+
+    def test_init_eb_event_base(self, eb_event_base):
+        assert isinstance(eb_event_base, EBEventBase)
+
+    def test_is_valid(self, eb_event_base_dict):
+        assert EBEventBase.is_valid(eb_event_base_dict) is True
+
+    @pytest.mark.parametrize("invalid_event", [
+        "invalid type",
+        {},
+        {"invalid.key": ["arn:aws:events:rule/EBEventBase"]},
+        {"source": "invalid.source"},
+        {"source": "invalid.source", "resources": ["arn:aws:events:rule/AnotherEvent"]},
+        {"source": "aws.events", "resources": ["arn:aws:sns:rule/InvalidEvent"]}
+    ])
+    def test_is_valid_invalid_event(self, invalid_event):
+        invalid_event = {
+            "source": "aws.sns",
+            "resources": ["arn:aws:events:rule/EBEventBase"]
+        }
+        assert EBEventBase.is_valid(invalid_event) is False
+
+    def test_from_dict(self, eb_event_base_dict):
+        event = EBEventBase.from_dict(eb_event_base_dict, None)
+        assert isinstance(event, EBEventBase)
+        assert event.event == eb_event_base_dict
+
+    def test_from_dict_with_not_valid_event(self):
+        invalid_event = {
+            "source": "aws.sns",
+            "resources": ["arn:aws:sns:rule/EBEventBase"]
+        }
+        with pytest.raises(ValueError, match="Invalid event format for EBEventBase"):
+            EBEventBase.from_dict(invalid_event, None)
